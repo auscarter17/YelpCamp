ba05c8899d024a89ad8da798833415b0012f97b0
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-10-04 14:47:54 -0400

    added flash to show success alerts upon editing and submitting campgrounds, submitting reviews

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const { response } = require('express');
-const Review = require('./models/review');
-
+const session = require('express-session');
+const flash = require('connect-flash');
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 
@@ -32,9 +30,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const sessionConfig = {
+  secret: 'thisshouldbeabettersecret!',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+app.use(session(sessionConfig))
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
+
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.get('/', (req, res) => {
   res.render('home')
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -29,6 +29,7 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
@@ -45,6 +46,7 @@ router.get('/:id/edit', catchAsync(async(req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  req.flash('success', 'Successfully updated campground!');
   res.redirect(`/campgrounds/${campground._id}`);
 }));
 
