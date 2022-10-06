6ae020080ea2883824ac0e311a73d4a7e7d8ecfa
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-10-06 15:19:22 -0400

    created controllers for user and review, simplified routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -12,43 +12,12 @@ router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-router.get('/:id', catchAsync(async(req, res) => {
-  const campground = await Campground.findById(req.params.id).populate({ 
-    path:'reviews',
-    populate: {
-      path:'author'
-    }
-    }).populate('author');
-  console.log(campground);
-  if (!campground) {
-    req.flash('error', 'Cannot find that campground!');
-    return res.redirect('/campgrounds');
-  }
-  res.render('campgrounds/show', { campground });
-}));
+router.get('/:id', catchAsync(campgrounds.showCampground));
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async(req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id)
-  if (!campground) {
-    req.flash('error', 'Cannot find that campground!');
-    return res.redirect('/campgrounds');
-  }
-  res.render('campgrounds/edit', { campground });
-}));
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-  req.flash('success', 'Successfully updated campground!');
-  res.redirect(`/campgrounds/${campground._id}`);
-}));
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  req.flash('success', 'Successfully deleted campground!');
-  res.redirect('/campgrounds');
-}));
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
